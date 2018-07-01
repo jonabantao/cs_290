08c247ca46f9c7c4ccaa3c54f10ceb704512bf00
Jonathan Abantao <jonabantao@gmail.com>
2018-07-01 00:33:48 -0700

    Complete functions activity

---
+++ week2/functions.js
@@ -0,0 +1,22 @@
+// Write a JavaScript program that declares a function but calls it 
+// before it is declared. Because of function hoisting this will work 
+// in JavaScript. 
+
+hoistedFunc();
+
+function hoistedFunc() {
+  console.log('Hey I\'m a function that was written after I was called!');
+}
+
+// Also write a function which is assigned to a variable. 
+// Call it before it is assigned and prove that this does not work.
+
+try {
+  hoistedFuncExpression();
+} catch(e) {
+  console.log('Cannot call a function expression before assigning it!');
+}
+
+const hoistedFuncExpression = () => {
+  console.log('This log should not be shown on runtime!');
+};
