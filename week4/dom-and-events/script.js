97a6851edde8298b838d5be0866c950903d6fc7d
Jonathan Abantao <jonabantao@gmail.com>
2018-07-16 01:39:19 -0700

    Create table using pure JavaScript

---
+++ week4/dom-and-events/script.js
@@ -0,0 +1,56 @@
+function buildPage() {
+  const NUM_OF_ROWS = 4;
+  const NUM_OF_COLS = 4;
+
+  const table = createTable(NUM_OF_ROWS, NUM_OF_COLS);
+  attachToBody(table);
+}
+
+function attachToBody(node) {
+  document.body.appendChild(node);
+}
+
+function createTable(rows, cols) {
+  const table = document.createElement('table');
+
+  appendTableHeader(table, cols);
+  appendTableBody(table, rows - 1, cols);
+
+  return table;
+}
+
+function appendTableHeader(tableElement, cols) {
+  const tableHead = document.createElement('thead');
+  const tableRow = document.createElement('tr');
+  
+  for (let i = 0; i < cols; i++) {
+    const tableHeaderCell = document.createElement('th');
+
+    tableHeaderCell.innerText = `Header ${i + 1}`;
+    tableRow.appendChild(tableHeaderCell);
+  }
+
+  tableHead.appendChild(tableRow);
+  tableElement.appendChild(tableHead);
+}
+
+function appendTableBody(tableElement, rows, cols) {
+  const tableBody = document.createElement('tbody');
+
+  for (let row = 0; row < rows; row++) {
+    const tableRow = document.createElement('tr');
+
+    for (let col = 0; col < cols; col++) {
+      const tableCell = document.createElement('td');
+
+      tableCell.innerText = `${row + 1} ${col + 1}`;
+      tableRow.appendChild(tableCell);
+    }
+
+    tableBody.appendChild(tableRow);
+  }
+
+  tableElement.appendChild(tableBody);
+}
+
+document.addEventListener('DOMContentLoaded', buildPage);
